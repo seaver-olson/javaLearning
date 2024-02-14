7663b9b720b1c0a3f48d97cd839e397db06305a9
Seaver Olson <sjolson007@gmail.com>
2024-02-14 14:41:42 -0600

    added test for server

---
+++ networking/server.java
@@ -0,0 +1,43 @@
+package javaLearning.networking;
+
+
+//made for custom server options
+public class server {
+    private int port;
+    private String ip;
+    private String name;
+
+    public server(int port, String ip, String name) {
+        this.port = port;
+        this.ip = ip;
+        this.name = name;
+    }
+
+    //getters
+    public int getPort() {
+        return port;
+    }
+    public String getIp() {
+        return ip;
+    }
+    public String getName() {
+        return name;
+    }
+    //setters
+    public void setPort(int port) {
+        this.port = port;
+    }
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "server{ip='" + ip + "', name='" + name + "', port=" + port + "}";
+    }
+
+    
+}
---
+++ networking/tester.java
@@ -0,0 +1,8 @@
+package javaLearning.networking;
+
+public class tester {
+    public static void main(String[] args) {
+        server s = new server(8080, "10.24.51.173", "server");
+        System.out.println(s);
+}
+}
