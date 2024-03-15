0b4fe4a41bcc8ddbc93d3be22ca334da7710d16d
seaver-olson <sjolson007@gmail.com>
2024-03-15 16:06:46 -0500

    left off on modifying fetch file

--- gui/fetchFile.java
+++ gui/fetchFile.java
@@ -1,32 +1,12 @@
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import javax.swing.*;
 
 
-public class fetchFile {
-    //using username, fetch file from server
-    public fetchFile(String username){
-        System.out.println("Fetching file for " + username);
-        String directory = "userData/" + username + "/";
-        File folder = new File(directory);
-        File[] listOfFiles = folder.listFiles();
-        //create table to display files
-        String[][] data = new String[listOfFiles.length][2];
-        for (int i = 0; i < listOfFiles.length; i++){
-            data[i][0] = listOfFiles[i].getName();
-            data[i][1] = "Download";
-        }
-        String[] column = {"File Name", "Action"};
-        JTable table = new JTable(data, column);
-        table.setBounds(50, 50, 300, 200);
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(50, 50, 300, 200);
-        JFrame frame = new JFrame(username + "'s Files");
-        frame.setSize(400, 300);
-        frame.add(scrollPane);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        
-        
-    }  
-
+public class fetchFile{
+    //using username as a parameter find folder 
+    public fetchFile(String username, DataOutputStream out, DataInputStream in){
+        File file = new File("/Users/Admin/Desktop/Projects/data/" + username);
+    }
 }
--- gui/system.java
+++ gui/system.java
@@ -13,23 +13,45 @@ import java.io.File;
 public class system {
     private String IP;
     private int port;
+    private String libraryPath = "/Users/Admin/Desktop/Projects/data/";
 
     public system(String IP, int port){
+        //if libraryPath/ does not exist exit program
+        File dir = new File(libraryPath);
+        if (!dir.exists()){
+            JOptionPane.showMessageDialog(null, "Library Path does not exist");
+            System.exit(0);
+        }
         this.IP = IP;
         this.port = port;
     }
     //ports will be automatically set to 8080
     public system(){
+        File dir = new File(libraryPath);
+        if (!dir.exists()){
+            JOptionPane.showMessageDialog(null, "Library Path does not exist");
+            System.exit(0);
+        }
         this.IP = "127.0.0.1";
         this.port = 8080;
     }
     
     public system(String IP){
+        File dir = new File(libraryPath);
+        if (!dir.exists()){
+            JOptionPane.showMessageDialog(null, "Library Path does not exist");
+            System.exit(0);
+        }
         this.IP = IP;
         this.port = 8080;
     }
     
     public system(int port){
+        File dir = new File(libraryPath);
+        if (!dir.exists()){
+            JOptionPane.showMessageDialog(null, "Library Path does not exist");
+            System.exit(0);
+        }
         this.IP = "127.0.0.1";
         this.port = port;
     }
