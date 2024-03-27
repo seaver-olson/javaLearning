93853172ccea5ce20bda0d490446d1bdd4517369
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2024-03-27 14:29:48 -0500

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,11 +1,14 @@
-message = "";
+message=""
 if [ $# -eq 0 ]; then
     message="No message added"
 else
     message="$1"
 fi
-cd FTP;
-rm Admin/*.class; rm Client/*.class;
+
+cd FTP || exit 1
+rm Admin/*.class
+rm Client/*.class
+
 git add .
-git commit -m message
+git commit -m "$message"
 git push
